7acb82271280a8fbb1204b3024eb40a8ffeec97c
iruora <arrouurii@gmail.com>
2023-05-10 16:09:40 +0200

    feat: enable cors

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -3,6 +3,7 @@ import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
