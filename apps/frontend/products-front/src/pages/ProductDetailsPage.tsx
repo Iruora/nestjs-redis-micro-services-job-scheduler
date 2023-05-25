fe259e83592c84342f580cf32fe022046ddec259
iruora <arrouurii@gmail.com>
2023-05-25 09:29:21 +0200

    fix: get rid of non existing styles imports

--- apps/frontend/products-front/src/pages/ProductDetailsPage.tsx
+++ apps/frontend/products-front/src/pages/ProductDetailsPage.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
 import { IProduct } from '../types/product';
 import ProductDetails from '../components/ProductDetails';
-import classes from './ProductDetailsPage.module.css';
 
 export default function ProductDetailsPage() {
   const product = useLoaderData() as IProduct;
 
   return (
-    <div className={classes['product-container']}>
+    <div>
       <ProductDetails {...product} />
     </div>
   );
